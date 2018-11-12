f43dfd0f17477ac3d07e2b4a3b0d93dc771874b7
Snugglez <32309975+Snugglez@users.noreply.github.com>
2018-11-12 14:54:36 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 module.exports = function autoleadpass(d) {
 let enabled = true,
 	serverId = null,
-	playerId = null
+	playerId = null,
+	members = [],
+	randomNum = null
 
 d.command.add("passlead", {
 $default() {
@@ -12,30 +14,47 @@ d.command.message(`[${enabled ? 'enabled' : 'disabled'}].`)
 
 d.hook('S_PARTY_MEMBER_LIST', 7, (e) => {
 if (enabled) {
-randomNum = Math.floor(Math.random() * e.members.length)
+members = e.members
+randomNum = Math.floor(Math.random() * members.length)
 
-if (e.members[randomNum].playerId == d.game.me.playerId && e.members[randomNum].serverId == d.game.me.serverId || !e.members[randomNum].online) {
-randomNum = Math.floor(Math.random() * e.members.length)
+for(let i = 0; i < 50 && (members[randomNum].playerId == d.game.me.playerId && members[randomNum].serverId == d.game.me.serverId || !members[randomNum].online); i++) {
+randomNum = Math.floor(Math.random() * members.length);
 }
 
-serverId = e.members[randomNum].serverId
-playerId = e.members[randomNum].playerId
+serverId = members[randomNum].serverId
+playerId = members[randomNum].playerId
 
 if (e.leaderPlayerId == d.game.me.playerId && e.leaderServerId == d.game.me.serverId) {
+setTimeout(function() {
 d.send('C_CHANGE_PARTY_MANAGER', 2, {
 serverId: serverId,
 playerId: playerId
 })
+}, Math.floor(Math.random() * 1000) + 2000)
 }
 }
 })
 
 d.hook('S_CHANGE_PARTY_MANAGER', 2, (e) => {
+if (enabled) {
 if (e.playerId == d.game.me.playerId && e.serverId == d.game.me.serverId) {
+
+randomNum = Math.floor(Math.random() * members.length)
+
+for(let i = 0; i < 50 && (members[randomNum].playerId == d.game.me.playerId && members[randomNum].serverId == d.game.me.serverId || !members[randomNum].online); i++) {
+randomNum = Math.floor(Math.random() * members.length);
+}
+
+serverId = members[randomNum].serverId
+playerId = members[randomNum].playerId
+
+setTimeout(function() {
 d.send('C_CHANGE_PARTY_MANAGER', 2, {
 serverId: serverId,
 playerId: playerId
 })
+}, Math.floor(Math.random() * 1000) + 2000)
+}
 }
 })
 
